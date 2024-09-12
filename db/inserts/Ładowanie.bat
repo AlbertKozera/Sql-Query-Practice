35a685d7ed2872c31a6fa3d4bc96fde49cfdce41
AlbertKozera <albertkozera97@gmail.com>
2024-09-12 02:06:13 +0200

    init

---
+++ db/inserts/Ładowanie.bat
@@ -0,0 +1,14 @@
+cd
+sqlldr Alfu/honkong12345 control=Rozklad.txt
+sqlldr Alfu/honkong12345 control=Osoba.txt
+sqlldr Alfu/honkong12345 control=Firma.txt
+sqlldr Alfu/honkong12345 control=Pojazdy.txt
+sqlldr Alfu/honkong12345 control=Linia.txt
+sqlldr Alfu/honkong12345 control=Pracownik.txt
+sqlldr Alfu/honkong12345 control=Miasto.txt
+sqlldr Alfu/honkong12345 control=Dzien.txt
+sqlldr Alfu/honkong12345 control=Miesiac.txt
+sqlldr Alfu/honkong12345 control=Rok.txt
+sqlldr Alfu/honkong12345 control=Czas.txt
+sqlldr Alfu/honkong12345 control=PRZEJAZD.txt
+pause
---
+++ load.bat
@@ -0,0 +1,13 @@
+cd
+loadpsp -replace -user Alfu/honkong12345 "Menu_Glowne.psp"
+loadpsp -replace -user Alfu/honkong12345 "R1.psp"
+loadpsp -replace -user Alfu/honkong12345 "R2.psp"
+loadpsp -replace -user Alfu/honkong12345 "C1.psp"
+loadpsp -replace -user Alfu/honkong12345 "C2.psp"
+loadpsp -replace -user Alfu/honkong12345 "P1.psp"
+loadpsp -replace -user Alfu/honkong12345 "P2.psp"
+loadpsp -replace -user Alfu/honkong12345 "G1.psp"
+loadpsp -replace -user Alfu/honkong12345 "G2.psp"
+loadpsp -replace -user Alfu/honkong12345 "O1.psp"
+loadpsp -replace -user Alfu/honkong12345 "O2.psp"
+pause
